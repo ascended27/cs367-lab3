7ef886c3de6e81274c1025e4d621e40ea926125e
VikinKitten <jcanchante@gmail.com>
2017-05-01 18:48:42 -0400

    adding changes to mm_free for seglist

--- mm_free.c
+++ mm_free.c
@@ -4,7 +4,7 @@
 
 
 int placeInSeglist(mem_ptr m);
-void coalesce(mem_ptr m);
+mem_ptr coalesce(mem_ptr m);
 
 extern mem_ptr Heap;
 extern int numLists;
@@ -12,21 +12,21 @@ extern mem_ptr* segLists;
 
 
 
-  /* Input: pointer to a mem_rec 
-     Output:  None
-
-        You must coalesce this block with adjacent blocks if
-        appropriate. The new free block (if possible, coalesced with
-        the neighbors) must be added to the appropriate segregated
-        list. The appropriate segregated list is defined as the
-        smallest nominal size list that is still large enough to hold
-        the new free block.
+/* 	Input: pointer to a mem_rec 
+	Output:  None
+	
+	You must coalesce this block with adjacent blocks if
+    appropriate. The new free block (if possible, coalesced with
+	the neighbors) must be added to the appropriate segregated
+    list. The appropriate segregated list is defined as the
+    smallest nominal size list that is still large enough to hold
+    the new free block.
 
 	Before returning from mm_free, make sure that all free blocks, in all
 	segregated lists, are address-ordered (lower addresses first).
 
 	If the input pointer is null, call error_msg(2) and return.  
-  */
+*/
 void mm_free(mem_ptr m) {
 
 
@@ -36,70 +36,9 @@ void mm_free(mem_ptr m) {
 
 	//checking if there is a single list
 	if(numLists == 1){
-	
-		int size = 0;
-
-		temp->valid = 0;
-
-
-		if(temp->previous != NULL)
-			//if the previous node is free
-			if(temp->previous->valid == 0){
-			
-				//total size of the block of memory
-				size = temp->previous->address + temp->previous->size;
-
-				//if the total block size is equal to the address of temp
-				if(size == temp->address){
-
-					erase = m;
-					temp->previous->next = temp->next;
-					temp->previous->size = temp->size + temp->previous->size;
-					
-					if(temp->next){
-						temp->next->previous = temp->previous;
-						temp = temp->previous;
-					}
-
-					
-					if(erase != Heap){
-						free(erase);
-						erase = NULL;
-					}
-					
-				}
-			}
-		
-		if(temp->next != NULL)
-			//if the next node is free
-			if(temp->next->valid == 0){
-				//total size of the memory block
-				size = temp->address + temp->size;
-
-				//if the total block size is equal to the address of the next node				
-				if(size == temp->next->address){
-					erase = temp->next;
-					temp->size = temp->size + temp->next->size;
-					
-					if(temp->next->next){
-						temp->next->next->previous = temp;
-						temp->next = temp->next->next;
-					}else{
-						temp->next = NULL;
-					}
-
-					if(erase != Heap){
-						free(erase);
-						erase = NULL;
-					}
-				}
-			}
-	
+		coalesce(m);
 	}
 	else{
-		
-		mem_ptr temp = m;
-		mem_ptr erase = NULL;
 		
 		int size = 0;
 		temp->valid = 0;
@@ -119,8 +58,12 @@ void mm_free(mem_ptr m) {
 				Heap = NULL;
 		}	
 		
-		int found = placeInSeglist(temp);
+		int location = placeInSeglist(temp);
 		coalesce(temp);
+		
+		if(segLists[i]->maxSize < temp->size){
+			location = placeInSegList(temp);
+		}
 	
 	}
 
@@ -175,6 +118,73 @@ int placeInsegList(temp){
 	return found;
 }
 
+mem_ptr coalesce(mem_ptr m){
+	
+//temporary pointer
+	mem_ptr temp = m;
+	mem_ptr erase = NULL;
+	
+	int size = 0;
+
+	temp->valid = 0;
+
+
+	if(temp->previous != NULL)
+		//if the previous node is free
+		if(temp->previous->valid == 0){
+
+			//total size of the block of memory
+			size = temp->previous->address + temp->previous->size;
+
+			//if the total block size is equal to the address of temp
+			if(size == temp->address){
+
+				erase = m;
+				temp->previous->next = temp->next;
+				temp->previous->size = temp->size + temp->previous->size;
+					
+				if(temp->next){
+					temp->next->previous = temp->previous;
+					temp = temp->previous;
+				}
+
+					
+				if(erase != Heap){
+					free(erase);
+					erase = NULL;
+				}
+					
+			}
+		}
+		
+	if(temp->next != NULL)
+		//if the next node is free
+		if(temp->next->valid == 0){
+		//total size of the memory block
+			size = temp->address + temp->size;
+
+			//if the total block size is equal to the address of the next node				
+			if(size == temp->next->address){
+				erase = temp->next;
+				temp->size = temp->size + temp->next->size;
+					
+				if(temp->next->next){
+					temp->next->next->previous = temp;
+					temp->next = temp->next->next;
+				}
+				else{
+					temp->next = NULL;
+				}
+
+				if(erase != Heap){
+					free(erase);
+					erase = NULL;
+				}
+			}
+		}
+			
+	return temp;
+}
 
 
 
